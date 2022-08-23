483065b86186de939e827d0dc858a60b9e2b34bb
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-08-23 15:47:06 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <div className="contentmax_width my-auto flex items-center lg:py-0 py-0 md:py-10">
         {heroContent.map((items,index)=>{
             return (
-                <div key={index} className="contentmax_width h-5/6 md:flex items-center justify-between">
+                <div key={index} className="contentmax_width md:flex items-center justify-between">
                     <div className="flex flex-col items-start gap-4 md:gap-7 w-full md:w-1/2">
                         <h1 className="text-5xl md:text-5xl lg:text-6xl xl:text-7xl text-white title">{items.fields.title}</h1>
                         <h1 className="text-sm text-white font-light tracking-wider md:w-2/3 w-full">{items.fields.content}</h1>
